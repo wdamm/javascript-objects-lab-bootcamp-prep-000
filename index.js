cfd99e3074851283e65f00951e6e9feb4055c09c
William Damm <wdamm@powerhrg.com>
2020-07-28 19:11:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-delete object.key;
+delete object[key];
 return object;
 }
 
